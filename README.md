98fff2d068d755ef6cfaf6f0bc9c21745e7de0de
Andrew Jason Wong <andrewwong998@gmail.com>
2022-05-21 16:45:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Nicer HackerNews 
+
+Scripts injection so HackerNews is nicer to read.
+
+## Development
+
+Write stuff, then run `yarn build`.
+
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name     HackerNews rice
+// @version  1
+// @grant    none
+// @include https://news.ycombinator.com/*
+// ==/UserScript==
+
+type NodeOperationFn = (node: Element | HTMLElement) => void;
+
+const selectElement = (query: string) => document.querySelectorAll(query); 
+
+const processNodes = (nodeList: NodeListOf<Element | HTMLElement>, fn: NodeOperationFn | NodeOperationFn[]) => {
+  if (Array.isArray(fn)) { 
+    fn.forEach((f) => { 
+      for (let i = 0; i < nodeList.length; i++) {
+        f(nodeList[i]);
+      }
+    })
+  } else {
+    for (let i = 0; i < nodeList.length; i++) {
+      fn(nodeList[i]);
+    }
+  }
+}
+
+// Operations
+const updateStyle = (property: any, value: string) => { 
+  return (node: HTMLElement) => node.style[property] = value;
+}
+
+processNodes(selectElement('#hnmain'), updateStyle('width', '50%'));
+processNodes(selectElement('.comment'), updateStyle('fontSize', '12pt'));
+processNodes(selectElement('.users'), updateStyle('fontWeight', 'bold'));
+processNodes(selectElement('.spacer'), updateStyle('height', '12px'));
+processNodes(selectElement('.titlelink'), updateStyle('fontSize', '12px'));
