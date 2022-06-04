6d5f8f91d3ac87675d0873d64ca8f83faee6251c
Andrew Jason Wong <andrewwong998@gmail.com>
2022-06-04 09:26:12 +0100

    Add extra spacing to .spacer class
    * Minor refactoring
    * Add options to `processNodes` function

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Nicer HackerNews 
+# Nicer HackerNews
 
 Scripts injection so HackerNews is nicer to read.
 
@@ -7,4 +7,3 @@ Scripts injection so HackerNews is nicer to read.
 ## Development
 
 Write stuff, then run `yarn build`.
-
--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,32 @@
 
 type NodeOperationFn = (node: Element | HTMLElement) => void;
 
-const selectElement = (query: string) => document.querySelectorAll(query);
+type NodeFilterFn = (node: Element | HTMLElement) => boolean;
+
+interface ProcessNodesOptions {
+  filter?: NodeFilterFn;
+}
+
+const selectElement = (query: string): NodeListOf<Element | HTMLElement> =>
+  document.querySelectorAll(query);
 
 const processNodes = (
   nodeList: NodeListOf<Element | HTMLElement>,
-  fn: NodeOperationFn | NodeOperationFn[]
+  fn: NodeOperationFn | NodeOperationFn[],
+  options?: ProcessNodesOptions
 ) => {
-  if (Array.isArray(fn)) {
-    fn.forEach((f) => {
-      for (let i = 0; i < nodeList.length; i++) {
-        f(nodeList[i]);
-      }
-    });
-  } else {
-    for (let i = 0; i < nodeList.length; i++) {
-      fn(nodeList[i]);
+  if (nodeList.length !== 0) {
+    const nodes =
+      options && options.filter
+        ? Array.from(nodeList).filter(options.filter)
+        : Array.from(nodeList);
+
+    if (Array.isArray(fn)) {
+      fn.forEach((f) => {
+        nodes.forEach(f);
+      });
+    } else {
+      nodes.forEach(fn);
     }
   }
 };
@@ -30,38 +41,44 @@ const updateStyle = (property: any, value: string) => {
   return (node: HTMLElement) => (node.style[property] = value);
 };
 
-const addInteraction = (event, fn: (node: HTMLElement) => void) => { 
-  return (node: HTMLElement) => node.addEventListener(event, (e) => fn(node))
-}
+const addInteraction = (event, fn: (node: HTMLElement) => void) => {
+  return (node: HTMLElement) => node.addEventListener(event, (_) => fn(node));
+};
 
 const changes = [
   {
-    element: '#hnmain',
+    element: "#hnmain",
     changes: [
-      updateStyle('width', '50%'),
-      updateStyle('backgroundColor', '#fff'),
+      updateStyle("width", "50%"),
+      updateStyle("backgroundColor", "#fff"),
     ],
   },
-  { element: '.comment', changes: updateStyle('fontSize', '12pt') },
-  { element: '.hnuser', changes: updateStyle('fontStyle', 'italic') },
-  { element: '.spacer', changes: updateStyle('height', '12px') },
-  { element: '.titlelink', changes: updateStyle('fontSize', '12pt') },
-  { element: '.score', changes: updateStyle('color', '#ff6600') },
-  { element: '.subtext', changes: updateStyle('fontSize', '9pt') },
+  { element: ".comment", changes: updateStyle("fontSize", "12pt") },
+  { element: ".hnuser", changes: updateStyle("fontStyle", "italic") },
+  { element: ".spacer", changes: updateStyle("height", "20px") },
+  { element: ".titlelink", changes: updateStyle("fontSize", "12pt") },
+  { element: ".score", changes: updateStyle("color", "#ff6600") },
+  { element: ".subtext", changes: updateStyle("fontSize", "9pt") },
   // Add background colour on hover
-  { element: '.default', changes: [ 
-    addInteraction('mouseover', updateStyle('backgroundColor', '#ededed')),
-    addInteraction('mouseleave', updateStyle('backgroundColor', '#fff')),
-  ] }
+  {
+    element: ".default",
+    changes: [
+      addInteraction("mouseover", updateStyle("backgroundColor", "#ededed")),
+      addInteraction("mouseleave", updateStyle("backgroundColor", "#fff")),
+    ],
+  },
+  {
+    element: "[href]",
+    changes: [updateStyle("fontWeight", "bold")],
+    options: {
+      filter: (node: Element) =>
+        /item\?id\=/.test(node.getAttribute("href")) &&
+        (/\d+\scomments?/.test(node.textContent) ||
+          /discuss/.test(node.textContent)),
+    },
+  },
 ];
 
-changes.forEach((c) => processNodes(selectElement(c.element), c.changes));
-
-Array.from(document.querySelectorAll('[href]'))
-  .filter(
-    (node) =>
-      /item\?id\=/.test(node.getAttribute('href')) &&
-      (/\d+\scomments?/.test(node.textContent) ||
-        /discuss/.test(node.textContent))
-  )
-  .forEach(updateStyle('fontWeight', 'bold'));
+changes.forEach((c) =>
+  processNodes(selectElement(c.element), c.changes, c.options)
+);
